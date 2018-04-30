64ef72b484421cbc85f04ad3412d0ea72e5b9ffd
leonixevious <jeremyrobin1997@hotmail.fr>
2018-04-30 12:30:38 +0200

    first commit

---
+++ serrure.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3.5
+#-*- coding: utf-8 -*-
+
+import time, re, os
+from pirc522 import RFID
+import RPi.GPIO as GPIO
+
+LED_RED = 7
+LED_GREEN = 5
+SERVO = 11
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+def init():
+    GPIO.setup(LED_RED, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(LED_GREEN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(SERVO, GPIO.OUT, initial=GPIO.LOW)
+
+def turn_high (gpio) :
+    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.output(gpio, GPIO.HIGH)
+
+def turn_low (gpio) :
+    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.output(gpio, GPIO.LOW)
+
+def scan_badge():
+    print("En attente de badge...")
+    rc522.wait_for_tag()
+    print("Badge detected")
+
+def add_log(ID, door):
+    os.chdir('/home/pi/')
+    fichier = open("log.txt", "a")
+    fichier.write('----------------------\n')
+    fichier.write('Badge : '+ID+'\n')
+    if door:
+        fichier.write('Badge Autorisé\n')
+    else:
+        fichier.write('Badge Refusé\n')
+    fichier.write(time.strftime("%A %d %B %Y %H:%M:%S")+"\n")
+    fichier.write('----------------------\n')
+    fichier.close()
+    print('>> Log ajouté')
+
+
+
+def setAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(11, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.5)
+
+def open_door():
+    pwm.start(0)
+    setAngle(180)
+    time.sleep(3)
+    setAngle(0)
+    time.sleep(1)
+    pwm.stop()
+
+init()
+
+rc522 = RFID()
+cardAuto = [
+'[6715944131115]',
+'[11511121729216]',
+'[23413518099186]'
+]
+
+
+
+while True :
+
+    scan_badge()
+
+    (error, tag_type) = rc522.request()
+
+    if not error :
+        (error, uid) = rc522.anticoll()
+
+        if not error :
+            print('UID : {}'.format(uid))
+            card = format(uid)
+            cardnow = str(re.sub('[, ]', '', card))
+            print('cardnow : '+cardnow)
+            i = 0
+            z = 0
+            for c in cardAuto:
+                i += 1 
+                if cardnow == c:
+                    add_log(cardnow, True)
+                    print("Autorizated")
+                    pwm=GPIO.PWM(11, 50)
+                    open_door()
+                    turn_high(LED_GREEN)
+                    time.sleep(3)
+                    turn_low(LED_GREEN)
+                elif i == len(cardAuto) and cardnow != c:
+                    add_log(cardnow, False)
+                    print("Nop poto")
+                    turn_high(LED_RED)
+                    time.sleep(3)
+                    turn_low(LED_RED)
+    turn_low(LED_RED)
+    turn_low(LED_GREEN)
---
+++ whatsmyip.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+"""Script that do a query to a server to renseign internal ip and hostname
+"""
+import socket
+import fcntl
+import struct
+import urllib2
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+server_script_url = 'http://plebweb.fr/cours-iot/whatsmyip.php'
+interface_name = 'wlan0'
+
+hostname = socket.gethostname()
+local_ip = get_ip_address(interface_name)
+
+request_url = server_script_url + '?local_ip=' + local_ip + '&hostname=' + hostname
+
+urllib2.urlopen(request_url).read()
